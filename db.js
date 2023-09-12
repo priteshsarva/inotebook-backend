bfafb1166dc48a0389f8fffd03c164335e6a5488
priteshsarva <priteshsarva9825@gmail.com>
2023-09-12 19:13:21 +0530

    bit updated

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+const Mongoose = require('mongoose');
 const mongooseURI="mongodb://127.0.0.1:27017/inotebook";
 
 const connectToMongo= async ()=>{
-       await mongoose.connect(mongooseURI);
+       await Mongoose.connect(mongooseURI);
 }
 
 module.exports = connectToMongo;
